d36ae500d21a70efedf75228a6ab41c292f61aaa
Mike-Bar <mikhail.baranov.webdev@gmail.com>
2021-02-14 23:14:36 +0100

    First commit installation and homepage controller

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomePageController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="homepage")
+     *
+     */
+    public function home()
+    {
+        
+
+        return $this->render(
+            'home.html.twig',
+        );
+    }
+}
